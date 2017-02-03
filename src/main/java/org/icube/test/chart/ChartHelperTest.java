08732c20704ba4198805aa10648b562f0f981901
rashmi <rashmi@i-cube.in>
2017-02-03 15:28:19 +0530

    updated junits with assert instead of print

--- src/main/java/org/icube/test/chart/ChartHelperTest.java
+++ src/main/java/org/icube/test/chart/ChartHelperTest.java
@@ -1,5 +1,7 @@
 package org.icube.test.chart;
 
+import static org.junit.Assert.*;
+
 import java.util.List;
 
 import org.icube.chart.Chart;
@@ -18,9 +20,9 @@ public class ChartHelperTest {
 	public void testGetQuestionForTab(){
 		List<Question> questionList = ch.getQuestionForTab(tabId);
 		for(Question q : questionList){
-			System.out.println("PageId : " + q.getPageId());
-			System.out.println("TabId :  " + q.getTabId());
-			System.out.println("Question :  " + q.getQuestionText());
+			assertNotNull("PageId : " + q.getPageId());
+			assertNotNull("TabId :  " + q.getTabId());
+			assertNotNull("Question :  " + q.getQuestionText());
 			
 		}
 	}
@@ -30,11 +32,11 @@ public class ChartHelperTest {
 	public void testGetChartMapping(){
 		List<Chart> chartList = ch.getChartMapping(pageId);
 		for(Chart c : chartList){
-			System.out.println("ChartId : " + c.getChartId());
-			System.out.println("Chart type :  " + c.getChartType());
-			System.out.println("Metric Id :  " + c.getMetricId());
-			System.out.println("Metric : " + c.getMetric());
-			System.out.println("Chart Title : " + c.getChartTitle());
+			assertNotNull("ChartId : " + c.getChartId());
+			assertNotNull("Chart type :  " + c.getChartType());
+			assertNotNull("Metric Id :  " + c.getMetricId());
+			assertNotNull("Metric : " + c.getMetric());
+			assertNotNull("Chart Title : " + c.getChartTitle());
 	}
 	}
 	
@@ -42,27 +44,27 @@ public class ChartHelperTest {
 	public void testGetChartDataForPage(){
 		List<Metric> metricList = ch.getChartDataForPage(pageId);
 		for(Metric m : metricList){
-			System.out.println("MetricId1 : " + m.getMetricId1());
-			System.out.println("MetricName1 : " + m.getMetricName1());
-			System.out.println("MetricId2 :  " + m.getMetricId2());
-			System.out.println("MetricName2 : " + m.getMetricName2());
-			System.out.println("MetricId3 :  " + m.getMetricId3());
-			System.out.println("MetricName3 : " + m.getMetricName3());
-			System.out.println("MetricId4 : " + m.getMetricId4());
-			System.out.println("MetricName4 : " + m.getMetricName4());
-			System.out.println("MetricId5 : " + m.getMetricId5());
-			System.out.println("MetricName5 : " + m.getMetricName5());
-			System.out.println("MetricId6 : " + m.getMetricId6());
-			System.out.println("MetricName6 : " + m.getMetricName6());
-			System.out.println("MetricId7 : " + m.getMetricId7());
-			System.out.println("MetricName7 : " + m.getMetricName7());
-			System.out.println("MetricId8 : " + m.getMetricId8());
-			System.out.println("MetricName8 : " + m.getMetricName8());
-			System.out.println("MetricId9 : " + m.getMetricId9());
-			System.out.println("MetricName9 : " + m.getMetricName9());
-			System.out.println("MetricId10 : " + m.getMetricId10());
-			System.out.println("MetricName10 : " + m.getMetricName10());
-			System.out.println("Candidate count : " + m.getCandidateCount());
+			assertNotNull("MetricId1 : " + m.getMetricId1());
+			assertNotNull("MetricName1 : " + m.getMetricName1());
+			assertNotNull("MetricId2 :  " + m.getMetricId2());
+			assertNotNull("MetricName2 : " + m.getMetricName2());
+			assertNotNull("MetricId3 :  " + m.getMetricId3());
+			assertNotNull("MetricName3 : " + m.getMetricName3());
+			assertNotNull("MetricId4 : " + m.getMetricId4());
+			assertNotNull("MetricName4 : " + m.getMetricName4());
+			assertNotNull("MetricId5 : " + m.getMetricId5());
+			assertNotNull("MetricName5 : " + m.getMetricName5());
+			assertNotNull("MetricId6 : " + m.getMetricId6());
+			assertNotNull("MetricName6 : " + m.getMetricName6());
+			assertNotNull("MetricId7 : " + m.getMetricId7());
+			assertNotNull("MetricName7 : " + m.getMetricName7());
+			assertNotNull("MetricId8 : " + m.getMetricId8());
+			assertNotNull("MetricName8 : " + m.getMetricName8());
+			assertNotNull("MetricId9 : " + m.getMetricId9());
+			assertNotNull("MetricName9 : " + m.getMetricName9());
+			assertNotNull("MetricId10 : " + m.getMetricId10());
+			assertNotNull("MetricName10 : " + m.getMetricName10());
+			assertNotNull("Candidate count : " + m.getCandidateCount());
 		}
 	}
 	
